7dcae0b14f4fed8ea5afca792bc257d044256b41
valentinrosier1903 <valentinrosier1903@hotmail.com>
2023-02-08 15:32:05 +0300

    first

--- src/App.js
+++ src/App.js
@@ -1,51 +1,12 @@
-import "./App.css";
-
-import {
-	BrowserRouter as Router,
-	Switch,
-	Route,
-	NavLink,
-} from "react-router-dom";
-
-// pages
-import Home from "./components/Home";
-import About from "./components/About";
-import Users from "./components/Users";
-import Error404 from "./components/Error404";
+import logo from './logo.svg';
+import './App.css';
 
 function App() {
-	return (
-		<Router>
-			<div>
-				<nav>
-					<ul>
-						<li>
-							<NavLink to="/" exact activeClassName="active">
-								Home
-							</NavLink>
-						</li>
-						<li>
-							<NavLink to="/about" activeClassName="active">
-								About
-							</NavLink>
-						</li>
-						<li>
-							<NavLink to="/users" activeClassName="active">
-								Users
-							</NavLink>
-						</li>
-					</ul>
-				</nav>
-
-				<Switch>
-					<Route path="/" exact component={Home} />
-					<Route path="/about" component={About} />
-					<Route path="/users" component={Users} />
-					<Route path="*" component={Error404} />
-				</Switch>
-			</div>
-		</Router>
-	);
+  return (
+    <div className="App">
+     <h1>Selam </h1>
+    </div>
+  );
 }
 
 export default App;
